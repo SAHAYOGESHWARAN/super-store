1af9231de8603d711e9d9f29198c122a632780b8
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-02 11:41:51 +0530

    store

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/murugan-store', {
+mongoose.connect('mongodb+srv://sahayogeshwaran1:6ZhJ9etupj6fptdN@cluster1.fpl98.mongodb.net/muruganStore', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('Database connected'))
