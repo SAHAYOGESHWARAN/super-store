434719ca51ad329e717de58aae7785f1d4c9c4ff
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-02 11:10:41 +0530

    store

--- backend/app.js
+++ backend/app.js
@@ -17,5 +17,6 @@ mongoose.connect('mongodb://localhost:27017/murugan-store', {
 }).then(() => console.log('Database connected'))
   .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
