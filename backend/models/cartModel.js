15dbe32c261b6304fe5a0e5357bad006cecc34c8
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-03 18:10:39 +0530

    store

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', required: true },
     products: [
         {
             product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
