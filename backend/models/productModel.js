b57c3c51b03c4fb9061be834c6bcd851a3c49b4c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-03 18:11:12 +0530

    store

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: { type: String, required: false }
+    name: { type: String, 
+        required: true },
+    description: { type: String, 
+        required: true },
+    price: { type: Number,
+         required: true },
+    image: { type: String,
+         required: false }
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
